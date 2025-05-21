ae918832a1b901b438e017986b2c004aaef1637b
AvrahamMeyers <a32100meyers@gmail.com>
2025-05-21 16:17:01 +0300

    changed point to be pair<int,int> added enum for pixel colors

--- find_line_in_image.hpp
+++ find_line_in_image.hpp
@@ -1,10 +1,16 @@
 #pragma once
 #include <vector>
-#include <array>
+#include <functional>
 
 using image = std::vector<std::vector<int>>;
-using point = std::array<int, 2>;
+using point = std::pair<int, int>;
 using points = std::vector<point>;
+
+enum PixelColors
+{
+    ZERO = 0,
+    ONE
+};
 
 enum Edge
 {
@@ -17,3 +23,5 @@ enum Edge
 const point find_point_in_edge(const image &img, const Edge edge);
 
 const points find_line_in_image(const image &img);
+
+const point search_for_point_in_line(const image &img, const int length, std::function<point(int)> mapIndex);
