12e156c24b52a086d49c769f870e2bdcd2350353
AvrahamMeyers <a32100meyers@gmail.com>
2025-05-21 11:48:54 +0300

    added enum for the sides of the image

--- find_line_in_image.hpp
+++ find_line_in_image.hpp
@@ -6,6 +6,14 @@ using image = std::vector<std::vector<int>>;
 using point = std::array<int, 2>;
 using points = std::vector<point>;
 
-const point find_point_in_edge();
+enum Edge
+{
+    TOP,
+    BOTTOM,
+    LEFT,
+    RIGHT
+};
+
+const point find_point_in_edge(const image &img, );
 
 const points find_line_in_image(const image &img);
