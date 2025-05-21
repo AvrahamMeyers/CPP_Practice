10b36c58df0f2e03c39c3ab5a43e532baa2ef582
AvrahamMeyers <a32100meyers@gmail.com>
2025-05-21 11:46:57 +0300

    adding type alias for a single point converted alias for multiple points to use the alias for a single point added function signature for finding the point in the edge

--- find_line_in_image.hpp
+++ find_line_in_image.hpp
@@ -3,6 +3,9 @@
 #include <array>
 
 using image = std::vector<std::vector<int>>;
-using points = std::vector<std::array<int, 2>>;
+using point = std::array<int, 2>;
+using points = std::vector<point>;
+
+const point find_point_in_edge();
 
 const points find_line_in_image(const image &img);
